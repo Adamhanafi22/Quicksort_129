f7fd891fca6ca95404ddc1db0c6ff8fe625c8da4
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-03-31 10:57:13 +0700

    Add project files.

---
+++ Quicksort_129/Quicksort_129.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//Array of integress to hold values
+int arr[20];
+int cmp_count = 0; //number of comparasion
+int mov_count = 0; //number of movement
+int n;
+void input() {
+	while (true)
+	{
+		cout << "masukkan panjang element array:";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "n\ Maksimum panjang array adalah 20 " << endl;
+
+	}
+
+}
